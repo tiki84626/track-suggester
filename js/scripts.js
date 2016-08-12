11312157ab0002c9d347e0a826a16cebd722c5d7
tiki84626 <andymalkin@gmail.com>
2016-08-12 14:33:19 -0700

    add track suggestions for android and business applications

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,47 @@ $(document).ready(function() {
     }
   });
 
-
   $("form#back-form").submit(function(event) {
+    var smallBigCompany = $("input:radio[name=small-big]:checked").val();
+
     event.preventDefault();
     $(".questionnaire").css("display", "none");
+    $(".question1-back").css("display", "none");
 
+    if (smallBigCompany === "small") {
+      $(".question2-small").show();
+    } else {
+      $(".question2-big").show();
+    }
   });
 
+  $("form#small-form").submit(function(event) {
+    var productType = $("input:radio[name=product-type]:checked").val();
+
+    event.preventDefault();
+    $(".questionnaire").css("display", "none");
+    $(".question2-small").css("display", "none");
+
+    if (productType === "web") {
+      $(".question3-web").show();
+    } else {
+      $(".question3-android").show();
+    }
+  });
+
+  $("form#big-form").submit(function(event) {
+    var productTypeBig = $("input:radio[name=product-type-big]:checked").val();
+
+    event.preventDefault();
+    $(".questionnaire").css("display", "none");
+    $(".question2-big").css("display", "none");
+
+    if (productTypeBig === "web") {
+      $(".question3-web").show();
+    } else if (productTypeBig === "business"){
+      $(".question3-business").show();
+    } else {
+      $(".question3-android").show();
+    }
+  });
 });
